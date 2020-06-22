93a9b6e949aa0bb53379e4293af469d36e6cc8b0
anthoine <anthoine@etu.u-pec.fr>
2020-06-22 16:13:28 +0200

    test

--- Code/js/diaporama.js
+++ Code/js/diaporama.js
@@ -2,7 +2,7 @@ var n_image = 0;
 var n_max_images = 0;
 var next_image = function (){
     n_image++;
-    if(n_image>n_max_images){
+    if(n_image>=n_max_images){
         n_image = 0;
     }
     refresh_image();
@@ -14,17 +14,27 @@ var previous_image = function (){
     }
     refresh_image();
 }
-var images;
-var textes;
+var images =[];
+var textes = [];
 var refresh_image = function (){
-    var images = document.getElementById("diaporama_images").children;
     document.getElementById("diaporama").style.backgroundImage="url("+ images[n_image].src +")";
+    document.getElementById("texte").innerText = textes[n_image].innerText;
+
 }
 
 window.addEventListener('load', function (){
     var all = document.getElementById("diaporama_images").children;
     for(var i=0; i<all.length ; i++){
-        if(all[i])
+        console.log(all[i]);
+        if(all[i].tagName === "IMG"){
+            console.log("image");
+            images.push(all[i]);
+        }else if(all[i].tagName === "P"){
+            console.log("p");
+            textes.push(all[i]);
+        }
+
     }
-    n_max_images = (document.getElementById("diaporama_images").children.length/2)-1;
+    n_max_images = images.length;
+    console.log(n_max_images);
 });
